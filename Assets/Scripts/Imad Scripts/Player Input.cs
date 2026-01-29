3d73cc9b4a8fc05870fbd8c9c68339fe932d4c6d
xilorez-ux <71128385+xilorez-ux@users.noreply.github.com>
2026-01-29 18:57:12 +0100

    input inbtern

--- Assets/Scripts/Imad Scripts/Player Input.cs
+++ Assets/Scripts/Imad Scripts/Player Input.cs
@@ -6,16 +6,12 @@ using UnityEngine.Rendering;
 
 public class PlayerInput : MonoBehaviour
 {
-    
-
     [SerializeField]
     internal InputSystem_Actions inputActions;
     [SerializeField] private float speed, accel;
 
     private float yPlayerAngle, xCameraAngle = 0f;
     [SerializeField] private float xRotationSpeed, yRotationSpeed = 1f;
-
-
 
     [SerializeField] private Rigidbody rb;
 
