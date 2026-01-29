f46fc7cd5c323afd5e993bbd379a583844ed3210
xilorez-ux <71128385+xilorez-ux@users.noreply.github.com>
2026-01-29 23:19:06 +0100

    Reduced issues with Basket mini-game
    Used IEnumerator with WaitFor 5 seconds

--- Assets/Scripts/Imad Scripts/Basket/LaunchBall.cs
+++ Assets/Scripts/Imad Scripts/Basket/LaunchBall.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -6,9 +7,19 @@ using UnityEngine.UI;
 public class LaunchBall : MonoBehaviour
 {
     private InputSystem_Actions inputActions;
+
+
+    [SerializeField] Slider verticalSlider;
+    [SerializeField] Slider horizontalSlider; 
+
     private Rigidbody rb;
 
     private bool coroutineRunning = false;
+    private bool secondCoroutineRunning = false;
+
+    [SerializeField] float sliderVerticalDirection = 0f;
+    [SerializeField] float sliderHorizontalDirection;
+    private Vector2 direction;
 
     private void Start()
     {
@@ -17,36 +28,77 @@ public class LaunchBall : MonoBehaviour
 
         inputActions = new InputSystem_Actions();
         inputActions.Enable();
+
+    }
+
+    private void Reset()
+    {
+        verticalSlider = GetComponent<Slider>();
+        horizontalSlider = GetComponent<Slider>();
     }
 
     private void Update()
     {
-        if (inputActions.Player.Interact.IsPressed() && coroutineRunning == false)
+        //if (verticalSlider.value <= 0f) {verticalSlider.value += 0.1f;}
+        //if (horizontalSlider.value <= 0f) {horizontalSlider.value += 0.1f;}
+
+        //if (verticalSlider.value >= 1f) { verticalSlider.value -= 0.1f;}
+        //if (horizontalSlider.value >= 1f) { horizontalSlider.value -= 0.1f;}
+
+        //direction = Quaternion.AngleAxis(90, Vector3.forward) * new Vector2(verticalSlider.value, horizontalSlider.value);
+
+        //if (inputActions.Player.Interact.IsPressed())
+        //{
+
+        //    rb.MovePosition(direction * Time.deltaTime);
+        //}
+
+        if (inputActions.Player.Interact.IsPressed())
         {
+
             StartCoroutine(MoveBall());
         }
     }
 
 
-    IEnumerator MoveBall() {
-           
-  
+    IEnumerator MoveBall()
+    {
+
         coroutineRunning = true;
         Debug.Log("IsPressed Déclenché");
 
-        for (int i = 0; i < 90; i++)
+        for (int i = 0; i < 80; i++)
         {
             rb.MovePosition(transform.position + (this.transform.forward * (-i) + this.transform.up * i) * Time.deltaTime);
             rb.useGravity = true;
         }
 
+
+        yield return new WaitForSeconds(0.5f);
+
+        for (int j = 0; j < 20; j++)
+        {
+            rb.MovePosition(transform.position + this.transform.forward * (-j) * Time.deltaTime);
+        }
+        //if(secondCoroutineRunning == false)
+        //{
+        //    StartCoroutine(MoveBallSecondStep());
+        //}
+
+
+        coroutineRunning = false;
+        yield return null;
+
+    }
+
+    IEnumerator MoveBallSecondStep()
+    {
         for (int j = 0; j < 20; j++)
         {
             rb.MovePosition(transform.position + this.transform.forward * (-j) * Time.deltaTime);
         }
 
-        coroutineRunning = false;
+        secondCoroutineRunning = false;
         yield return null;
-        
     }
 }
